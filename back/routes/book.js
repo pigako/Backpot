2ab219aa6af933b27c3476c0637771e4fde80704
pigako <kororo1019@gmail.com>
2020-03-20 17:49:47 +0900

    Making backend route patch Book

--- back/routes/book.js
+++ back/routes/book.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const db = require('../models');
-const { isLoggedIn } = require('./middleware');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const db = require("../models");
+const { isLoggedIn } = require("./middleware");
 
 const router = express.Router();
 
@@ -10,41 +10,41 @@ const router = express.Router();
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
-      cb(null, 'uploads');
+      cb(null, "uploads");
     },
     filename(req, file, cb) {
       const ext = path.extname(file.originalname);
       const basename = path.basename(file.originalname, ext);
       cb(null, basename + new Date().valueOf() + ext);
     },
-    limits: { fileSize: 20 * 1024 * 1024 },
-  }),
+    limits: { fileSize: 20 * 1024 * 1024 }
+  })
 });
 // 책 정보 가져오기
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const book = await db.Book.findOne({
       where: {
-        id: parseInt(req.params.id, 10),
+        id: parseInt(req.params.id, 10)
       },
       include: [
         {
           model: db.User,
-          attributes: ['id', 'userId', 'nickname'],
+          attributes: ["id", "userId", "nickname"]
         },
         {
           model: db.User,
-          as: 'LikersBook',
+          as: "LikersBook"
         },
         {
-          model: db.Episode,
+          model: db.Episode
         },
         {
           model: db.Genre,
-          as: 'BookGenre',
-          attributes: ['id', 'name'],
-        },
-      ],
+          as: "BookGenre",
+          attributes: ["id", "name"]
+        }
+      ]
     });
     return res.json(book);
   } catch (e) {
@@ -53,19 +53,19 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 // 책 만들기
-router.post('/', isLoggedIn, async (req, res, next) => {
+router.post("/", isLoggedIn, async (req, res, next) => {
   try {
     const newBook = await db.Book.create({
       name: req.body.name,
       thumbnail: req.body.thumbnail,
       serialDay: req.body.serialDay,
       summary: req.body.summary,
-      UserId: req.user.id,
+      UserId: req.user.id
     });
     await Promise.all(
       req.body.genre.map(g => {
         newBook.addBookGenre(g);
-      }),
+      })
     );
     return res.json(newBook);
   } catch (e) {
@@ -73,20 +73,44 @@ router.post('/', isLoggedIn, async (req, res, next) => {
     return next(e);
   }
 });
+router.patch("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const book = await db.Book.findOne({
+      where: { id: parseInt(req.body.id, 10) }
+    });
+    if (!book) {
+      return res.status(404).send("존재하지 않는 작품입니다.");
+    }
+    if (book.UserId !== req.user.id) {
+      return res.status(404).send("수정 권한이 없는 작품입니다.");
+    }
+    await db.Book.update(
+      {
+        // 수정 채워넣기
+      },
+      {
+        where: { id: parseInt(req.body.id, 10) }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
+});
 // 선호작 등록
-router.post('/:id/like', isLoggedIn, async (req, res, next) => {
+router.post("/:id/like", isLoggedIn, async (req, res, next) => {
   try {
     const me = await db.User.findOne({
-      where: { id: req.user.id },
+      where: { id: req.user.id }
     });
     await me.addLikingBook(parseInt(req.params.id, 10));
     await db.Book.increment(
       {
-        likersCount: 1,
+        likersCount: 1
       },
       {
-        where: { id: parseInt(req.params.id, 10) },
-      },
+        where: { id: parseInt(req.params.id, 10) }
+      }
     );
     return res.send(req.params.id);
   } catch (e) {
@@ -95,19 +119,19 @@ router.post('/:id/like', isLoggedIn, async (req, res, next) => {
   }
 });
 // 선호작 취소
-router.delete('/:id/unlike', isLoggedIn, async (req, res, next) => {
+router.delete("/:id/unlike", isLoggedIn, async (req, res, next) => {
   try {
     const me = await db.User.findOne({
-      where: { id: req.user.id },
+      where: { id: req.user.id }
     });
     await me.removeLikingBook(parseInt(req.params.id, 10));
     await db.Book.increment(
       {
-        likersCount: -1,
+        likersCount: -1
       },
       {
-        where: { id: parseInt(req.params.id, 10) },
-      },
+        where: { id: parseInt(req.params.id, 10) }
+      }
     );
     res.send(req.params.id);
   } catch (e) {
@@ -116,14 +140,19 @@ router.delete('/:id/unlike', isLoggedIn, async (req, res, next) => {
   }
 });
 // 썸네일 이미지 업로드
-router.post('/thumbnail', isLoggedIn, upload.single('image'), (req, res, next) => {
-  try {
-    console.log('req.file', req.file);
-    res.json(req.file.filename);
-  } catch (e) {
-    console.log(e);
-    return next(e);
+router.post(
+  "/thumbnail",
+  isLoggedIn,
+  upload.single("image"),
+  (req, res, next) => {
+    try {
+      console.log("req.file", req.file);
+      res.json(req.file.filename);
+    } catch (e) {
+      console.log(e);
+      return next(e);
+    }
   }
-});
+);
 
 module.exports = router;
