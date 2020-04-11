2f1e6094ae52b5bca388e4c79ffe3ffc05224bae
pigako <kororo1019@gmail.com>
2020-04-11 20:10:48 +0900

    front config modify

--- front/config/config.js
+++ front/config/config.js
@@ -1,10 +1,10 @@
 const backUrl =
   process.env.NODE_ENV === 'production'
-    ? process.env.FRONT_URL
+    ? process.env.BACK_URL
     : 'http://loaclhost:5000';
 const frontUrl =
   process.env.NODE_ENV === 'production'
-    ? process.env.BACK_URL
+    ? process.env.FRONT_URL
     : 'http://localhost:8080';
 
 export { backUrl, frontUrl };
