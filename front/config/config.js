545e11d1181f93a33d2238443d4955d293be9dd8
pigako <kororo1019@gmail.com>
2020-04-11 19:20:13 +0900

    FRONT URL & BACK URL

--- front/config/config.js
+++ front/config/config.js
@@ -1,6 +1,10 @@
 const backUrl =
-  process.env.NODE_ENV === 'production' ? '' : 'http://loaclhost:5000';
+  process.env.NODE_ENV === 'production'
+    ? process.env.FRONT_URL
+    : 'http://loaclhost:5000';
 const frontUrl =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080';
+  process.env.NODE_ENV === 'production'
+    ? process.env.BACK_URL
+    : 'http://localhost:8080';
 
 export { backUrl, frontUrl };
